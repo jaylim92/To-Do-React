e3efb31f543436660125c24c2214e611b43d0093
jaylim92 <jaylim3272@gmail.com>
2022-08-17 21:59:50 +0900

    set Selector on atom

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,13 +1,25 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export interface IToDos {
+export interface IToDo {
     text: string;
     id: number;
     category: "TO_DO" | "Doing" | "Done";
   }
 
-export const toDoState = atom<IToDos[]>({
+export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
   });
   
+
+  export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({get})=> {
+      const toDos = get(toDoState)
+      return [
+        toDos.filter(toDo=> toDo.category === "TO_DO"),
+        toDos.filter(toDo=> toDo.category === "Doing"),
+        toDos.filter(toDo=> toDo.category === "Done"),
+      ]
+    }
+  })
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,38 @@
-import { IToDos } from "../atoms";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDos) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((prev) =>
+      prev.map((oldToDo) => {
+        return oldToDo.id === id
+          ? { text, id, category: name as any }
+          : oldToDo;
+      })
+    );
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "Doing" && (
+        <button name="Doing" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "Done" && (
+        <button name="Done" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
